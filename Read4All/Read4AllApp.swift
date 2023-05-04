2d8baeee6de314956d30bb9c56df478d41777677
Gois <matheusgoislimasilva@gmail.com>
2023-05-03 23:04:13 -0300

    Initial Commit

---
+++ Read4All/Read4AllApp.swift
@@ -0,0 +1,17 @@
+//
+//  Read4AllApp.swift
+//  Read4All
+//
+//  Created by Matheus Gois on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Read4AllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
